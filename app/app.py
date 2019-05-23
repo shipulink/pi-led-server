7a9448f260a5290c723eaaf832e364613b5e5b64
Konstantin <konman87@gmail.com>
2019-05-23 12:51:11 -0500

    using absolute path for library build script

--- app/app.py
+++ app/app.py
@@ -2,8 +2,10 @@ import subprocess
 import ctypes
 import os.path
 
-subprocess.call(["sh makeledlib.sh"])
-lib_abs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "ledlib.so"))
+current_dir = os.path.dirname(__file__)
+make_file_path = os.path.abspath(os.path.join(current_dir, "makeledlib.sh"))
+subprocess.call(["sh " + make_file_path])
+lib_abs_path = os.path.abspath(os.path.join(current_dir, "ledlib.so"))
 
 ledlib = ctypes.CDLL(lib_abs_path)
 
