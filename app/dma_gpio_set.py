013b7b63148c3f6972f94fdb21a69481fb95db77
Konstantin <konman87@gmail.com>
2019-08-06 09:12:52 -0500

    Now flipping pin 18 using the same CB DEST_ADDR

--- app/dma_gpio_set.py
+++ app/dma_gpio_set.py
@@ -13,14 +13,14 @@ GPCLR0_BUS = GPIO_BASE_BUS + 0x28
 cb_list = dma.build_linked_cb_list(2)
 
 cb_list[0].init_source_data(4)
-cb_list[0].write_word_to_source_data(0x0, 1<<24)
+cb_list[0].write_word_to_source_data(0x0, 1 << 24)
 cb_list[0].set_transfer_information(1 << 26)
 cb_list[0].set_destination_addr(GPFSEL_BUS)
 
-cb_list[1].init_source_data(4)
-cb_list[1].write_word_to_source_data(0x0, 1<<18)
-cb_list[1].set_transfer_information(1 << 26)
-cb_list[1].set_destination_addr(GPCLR0_BUS)
-# cb_list[1].set_destination_addr(GPSET0_BUS)
+cb_list[1].init_source_data(16)
+# cb_list[1].write_word_to_source_data(0x0, 1 << 18)  # Set pin 18
+cb_list[1].write_word_to_source_data(0xC, 1 << 18)  # Clear pin 18
+cb_list[1].set_transfer_information(1 << 26 | 1 << 8 | 1 << 4)
+cb_list[1].set_destination_addr(GPSET0_BUS)
 
 dma.activate_channel_with_cb(0, cb_list[0].addr)
