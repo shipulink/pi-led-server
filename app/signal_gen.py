6d466110f7212916425933a1ddfe62a8fe4bd656
Konstantin <konman87@gmail.com>
2019-10-11 08:38:54 -0500

    Pulling out some constants to the top, for convenience while testing / tuning. Now running a continuous CB wait loop, and interrupting it to send data. CB_UPD now switches between two separate CB flows - one for Zero, one for One.

--- app/signal_gen.py
+++ app/signal_gen.py
@@ -7,6 +7,15 @@ import mmap
 import app.dma as dma
 import app.memory_utils as mu
 
+SRC = 6  # 1 = Oscillator = 19.2MHz; 5 = PLLC = 1GHz; 6 = PLLD = 500MHz
+DIV = 128
+CYCLES = 128
+
+PLAY_SECONDS = 5
+DMA_CH = 0
+
+print(1000000000 / 500000000 * (DIV * CYCLES))
+
 # MMAP constants:
 MMAP_FLAGS = mmap.MAP_SHARED
 MMAP_PROT = mmap.PROT_READ | mmap.PROT_WRITE
@@ -29,7 +38,7 @@ DMA_NO_WIDE_BURSTS = 1 << 26
 DMA_PERMAP = 5 << 16  # 5 = PWM, 2 = PCM
 
 DMA_FLAGS = DMA_NO_WIDE_BURSTS | DMA_WAIT_RESP | DMA_WAITS
-PWM_DMA_FLAGS = DMA_PERMAP | DMA_DEST_DREQ | DMA_SRC_IGNORE | DMA_NO_WIDE_BURSTS
+PWM_DMA_FLAGS = DMA_PERMAP | DMA_DEST_DREQ | DMA_NO_WIDE_BURSTS | DMA_WAIT_RESP
 
 # PWM addresses
 PWM_BASE = 0x2020C000
@@ -38,7 +47,7 @@ PWM_CTL = 0x0  # PWM Control
 PWM_DMAC = 0x8  # DMA Configuration
 PWM_RNG1 = 0x10  # PWM Channel 1 Range (we'll only be using Channel 1)
 PWM_FIFO = 0x18  # FIFO Queue input. This is where we'll be writing dummy data.
-PWM_CYCLES = 16  # PWM period will be this many PWM clock cycles long
+PWM_CYCLES = CYCLES  # PWM period will be this many PWM clock cycles long
 
 # PWM constants
 # PWM constants
@@ -56,8 +65,8 @@ PWM_CLK_DIV = 0x4
 
 # PWM clock constants
 PWM_CLK_PWD = 0x5A << 24
-PWM_CLK_SRC = 6 << 0
-PWM_CLK_INT_DIV = 8 << 12  # Integer divisor. Clock rate will be SRC clock rate / this.
+PWM_CLK_SRC = SRC << 0
+PWM_CLK_INT_DIV = DIV << 12  # Integer divisor. Clock rate will be SRC clock rate / this.
 PWM_CLK_ENAB = 1 << 4  # Enable clock.
 
 
@@ -99,67 +108,96 @@ def populate_control_array(target_int_arr, src_bytes, ad_low, ad_high, ad_stop):
 
 
 # Build control array from incoming bytes
-ints = [0b11110111]
+ints = [0b10110110]
+# ints = [0b11111111]
+# ints = [0b0]
+# ints = [0b11110000]
 byte_arr = array.array("B", ints)
 ctl_arr = build_control_array(len(byte_arr))
 
-shared_mem = mu.ctypes_alloc_aligned(576, 32)  # 9 CBs. For each CB, 32 bytes for config, 32 bytes for data.
+# Allocate enough memory for all the CBs. For each CB, 32 bytes for config, 32 bytes for data.
+shared_mem = mu.ctypes_alloc_aligned(1024, 32)
 
-CB_WAIT1 = dma.ControlBlock2(shared_mem, 0x0)
-CB_LOW = dma.ControlBlock2(shared_mem, 0x40)
-CB_UPD = dma.ControlBlock2(shared_mem, 0x80)  # Needs stride. Updates its own src addr and CB_WAIT2's next CB addr
-CB_WAIT2 = dma.ControlBlock2(shared_mem, 0xC0)  # CB_UPD updates next CB addr of this CB
-CB_H_TOGGLE = dma.ControlBlock2(shared_mem, 0x100)  # next CB addr = ad(CB_WAIT3)
-CB_L_TOGGLE = dma.ControlBlock2(shared_mem, 0x140)  # next CB addr = ad(CB_WAIT3)
-CB_STOP = dma.ControlBlock2(shared_mem, 0x180)  # next CB addr = 0
-CB_WAIT3 = dma.ControlBlock2(shared_mem, 0x1C0)
-CB_HIGH = dma.ControlBlock2(shared_mem, 0x200)
+# CBs common to both the Zero and One flows.
+CB_WAIT = dma.ControlBlock2(shared_mem, 0x0)
+CB_WAIT_L = dma.ControlBlock2(shared_mem, 0x40)
+CB_UPD = dma.ControlBlock2(shared_mem, 0x80)  # Needs stride. Updates its own src addr and CB_DATA_WAIT's next CB addr
+CB_DATA_WAIT = dma.ControlBlock2(shared_mem, 0xC0)
+CB_STOP = dma.ControlBlock2(shared_mem, 0x100)
 
-CB_WAIT1.set_transfer_information(PWM_DMA_FLAGS)
-CB_WAIT1.set_destination_addr(PWM_BASE_BUS + PWM_FIFO)
-CB_WAIT1.set_next_cb(CB_LOW.addr)
+# CBs for representing a zero
+CB_ZERO_H1 = dma.ControlBlock2(shared_mem, 0x140)
+CB_ZERO_W1 = dma.ControlBlock2(shared_mem, 0x180)
+CB_ZERO_L1 = dma.ControlBlock2(shared_mem, 0x1C0)
 
-CB_LOW.set_transfer_information(DMA_FLAGS)
-CB_LOW.write_word_to_source_data(0x0, 1 << 18)  # pin 18
-CB_LOW.set_destination_addr(GPCLR0)
-CB_LOW.set_next_cb(CB_UPD.addr)
+# CBs for representing a one
+CB_ONE_H1 = dma.ControlBlock2(shared_mem, 0x200)
+CB_ONE_W1 = dma.ControlBlock2(shared_mem, 0x240)
+CB_ONE_W2 = dma.ControlBlock2(shared_mem, 0x280)
+CB_ONE_L1 = dma.ControlBlock2(shared_mem, 0x2C0)
+
+# Configure Common CBs
+CB_WAIT.set_transfer_information(PWM_DMA_FLAGS)
+CB_WAIT.set_destination_addr(PWM_BASE_BUS + PWM_FIFO)
+CB_WAIT.set_next_cb(CB_WAIT_L.addr)
+
+CB_WAIT_L.set_transfer_information(DMA_FLAGS)
+CB_WAIT_L.write_word_to_source_data(0x0, 1 << 18)  # pin 18
+CB_WAIT_L.set_destination_addr(GPCLR0)
+CB_WAIT_L.set_next_cb(CB_WAIT.addr)
 
 src_stride = int(len(ctl_arr) / 2) * 4
-dest_stride = CB_WAIT2.addr + 0x14 - (CB_UPD.addr + 0x4)
+dest_stride = CB_DATA_WAIT.addr + 0x14 - (CB_UPD.addr + 0x4)
 CB_UPD.set_transfer_information(DMA_FLAGS | DMA_TD_MODE)
 CB_UPD.set_source_addr(mu.virtual_to_physical_addr(ctypes.addressof(ctl_arr)).p_addr)
 CB_UPD.set_destination_addr(CB_UPD.addr + 0x4)
 CB_UPD.set_transfer_length_stride(4, 2)
 CB_UPD.set_stride(src_stride, dest_stride)
-CB_UPD.set_next_cb(CB_WAIT2.addr)
+CB_UPD.set_next_cb(CB_DATA_WAIT.addr)
 
-CB_WAIT2.set_transfer_information(PWM_DMA_FLAGS)
-CB_WAIT2.set_destination_addr(PWM_BASE_BUS + PWM_FIFO)
-
-CB_H_TOGGLE.set_transfer_information(DMA_FLAGS)
-CB_H_TOGGLE.write_word_to_source_data(0x0, 1 << 18)  # pin 18
-CB_H_TOGGLE.set_destination_addr(GPSET0)
-CB_H_TOGGLE.set_next_cb(CB_WAIT3.addr)
-
-CB_L_TOGGLE.set_transfer_information(DMA_FLAGS)
-CB_L_TOGGLE.write_word_to_source_data(0x0, 1 << 18)  # pin 18
-CB_L_TOGGLE.set_destination_addr(GPCLR0)
-CB_L_TOGGLE.set_next_cb(CB_WAIT3.addr)
+CB_DATA_WAIT.set_transfer_information(PWM_DMA_FLAGS)
+CB_DATA_WAIT.set_destination_addr(PWM_BASE_BUS + PWM_FIFO)
 
 CB_STOP.set_transfer_information(DMA_FLAGS)
-CB_STOP.write_word_to_source_data(0x0, 1 << 18)  # pin 18
-CB_STOP.set_destination_addr(GPCLR0)
+CB_STOP.write_word_to_source_data(0x0, CB_WAIT.addr)
+CB_STOP.set_destination_addr(CB_WAIT_L.addr + 0x14)
+CB_STOP.set_next_cb(CB_WAIT.addr)
 
-CB_WAIT3.set_transfer_information(PWM_DMA_FLAGS)
-CB_WAIT3.set_destination_addr(PWM_BASE_BUS + PWM_FIFO)
-CB_WAIT3.set_next_cb(CB_HIGH.addr)
+# Configure CBs for representing a zero
+CB_ZERO_H1.set_transfer_information(DMA_FLAGS)
+CB_ZERO_H1.write_word_to_source_data(0x0, 1 << 18)  # pin 18
+CB_ZERO_H1.set_destination_addr(GPSET0)
+CB_ZERO_H1.set_next_cb(CB_ZERO_W1.addr)
 
-CB_HIGH.set_transfer_information(DMA_FLAGS)
-CB_HIGH.write_word_to_source_data(0x0, 1 << 18)  # pin 18
-CB_HIGH.set_destination_addr(GPSET0)
-CB_HIGH.set_next_cb(CB_WAIT1.addr)
+CB_ZERO_W1.set_transfer_information(PWM_DMA_FLAGS)
+CB_ZERO_W1.set_destination_addr(PWM_BASE_BUS + PWM_FIFO)
+CB_ZERO_W1.set_next_cb(CB_ZERO_L1.addr)
 
-populate_control_array(ctl_arr, byte_arr, CB_L_TOGGLE.addr, CB_H_TOGGLE.addr, CB_STOP.addr)
+CB_ZERO_L1.set_transfer_information(DMA_FLAGS)
+CB_ZERO_L1.write_word_to_source_data(0x0, 1 << 18)  # pin 18
+CB_ZERO_L1.set_destination_addr(GPCLR0)
+CB_ZERO_L1.set_next_cb(CB_UPD.addr)
+
+# Configure CBs for representing a one
+CB_ONE_H1.set_transfer_information(DMA_FLAGS)
+CB_ONE_H1.write_word_to_source_data(0x0, 1 << 18)  # pin 18
+CB_ONE_H1.set_destination_addr(GPSET0)
+CB_ONE_H1.set_next_cb(CB_ONE_W1.addr)
+
+CB_ONE_W1.set_transfer_information(PWM_DMA_FLAGS)
+CB_ONE_W1.set_destination_addr(PWM_BASE_BUS + PWM_FIFO)
+CB_ONE_W1.set_next_cb(CB_ONE_W2.addr)
+
+CB_ONE_W2.set_transfer_information(PWM_DMA_FLAGS)
+CB_ONE_W2.set_destination_addr(PWM_BASE_BUS + PWM_FIFO)
+CB_ONE_W2.set_next_cb(CB_ONE_L1.addr)
+
+CB_ONE_L1.set_transfer_information(DMA_FLAGS)
+CB_ONE_L1.write_word_to_source_data(0x0, 1 << 18)  # pin 18
+CB_ONE_L1.set_destination_addr(GPCLR0)
+CB_ONE_L1.set_next_cb(CB_UPD.addr)
+
+populate_control_array(ctl_arr, byte_arr, CB_ZERO_H1.addr, CB_ONE_H1.addr, CB_STOP.addr)
 
 ad_info = mu.virtual_to_physical_addr(ctypes.addressof(ctl_arr))
 with open("/dev/mem", "r+b", buffering=0) as f:
@@ -184,13 +222,13 @@ clk_cb.set_transfer_information(DMA_NO_WIDE_BURSTS | DMA_WAIT_RESP | DMA_SRC_INC
 clk_cb.init_source_data(8)
 clk_cb.write_word_to_source_data(PWM_CLK_CTL, PWM_CLK_PWD | PWM_CLK_SRC)
 clk_cb.write_word_to_source_data(PWM_CLK_DIV, PWM_CLK_PWD | PWM_CLK_INT_DIV)
-dma.activate_channel_with_cb(0, clk_cb.addr)
+dma.activate_channel_with_cb(DMA_CH, clk_cb.addr)
 time.sleep(0.1)
 
 # Start PWM clock
 clk_cb.init_source_data(4)
 clk_cb.write_word_to_source_data(PWM_CLK_CTL, PWM_CLK_PWD | PWM_CLK_SRC | PWM_CLK_ENAB)
-dma.activate_channel_with_cb(0, clk_cb.addr)
+dma.activate_channel_with_cb(DMA_CH, clk_cb.addr)
 time.sleep(0.1)
 
 ###########################
@@ -213,20 +251,24 @@ cb_gpio_conf.init_source_data(4)
 cb_gpio_conf.write_word_to_source_data(0x0, 1 << 24)
 cb_gpio_conf.set_transfer_information(DMA_WAIT_RESP)
 cb_gpio_conf.set_destination_addr(GPFSEL1)
-dma.activate_channel_with_cb(0, cb_gpio_conf.addr)
+dma.activate_channel_with_cb(DMA_CH, cb_gpio_conf.addr)
 time.sleep(0.1)
 
 #############
 # Start DMA #
 #############
+dma.activate_channel_with_cb(DMA_CH, CB_WAIT.addr)
+time.sleep(0.1)
+
 start = time.time()
-while time.time() - start < 10:
-    dma.activate_channel_with_cb(6, CB_WAIT1.addr)
+while time.time() - start < PLAY_SECONDS:
+    CB_WAIT_L.set_next_cb(CB_UPD.addr)
+    # time.sleep(.01)
 
 ############
 # Stop PWM #
 ############
 clk_cb.init_source_data(4)
 clk_cb.write_word_to_source_data(PWM_CLK_CTL, PWM_CLK_PWD | PWM_CLK_SRC)
-dma.activate_channel_with_cb(0, clk_cb.addr)
+dma.activate_channel_with_cb(DMA_CH, clk_cb.addr)
 time.sleep(0.1)
